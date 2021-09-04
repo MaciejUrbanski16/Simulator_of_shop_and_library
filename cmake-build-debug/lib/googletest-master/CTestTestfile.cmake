fd7c52daa854455937f63cdf53a71c86e8f24903
Maciej U <urbanskimac@gmail.com>
2021-09-04 20:44:04 +0200

    Cleaning repository from debuf information

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/Desktop/Projekt/Simulator_of_shop/lib/googletest-master
-# Build directory: C:/Users/Admin/Desktop/Projekt/Simulator_of_shop/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
